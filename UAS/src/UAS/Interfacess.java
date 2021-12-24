87b300e9f974330fa9d569d69c5dc7653c90805d
Muhammad Fikri Wibowo <45762569+muhfikriw@users.noreply.github.com>
2021-12-25 05:02:20 +0700

    Add files via upload

---
+++ UAS/src/UAS/Interfacess.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UAS;
+
+/**
+ *
+ * @author Asus
+ */
+public interface Interfacess {
+    public void add(Data data);
+    public void delete(String idk);
+    public void search(String idk);
+    public void print();
+}
